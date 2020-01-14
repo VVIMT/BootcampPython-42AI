74f3855aa81574c75790b17e1e48c53a577bfc99
Vincent Vimont <vincent.vimont@live.fr>
2020-01-15 00:46:13 +0100

    day01

--- day01/ex00/book.py
+++ day01/ex00/book.py
@@ -1,19 +1,27 @@
 import sys
+import datetime
 from recipe import Recipe
 
 class Book(Recipe):
-    def __init__(name='', recipes_list={}):
+    name = ''
+    last_update=datetime.datetime.now()
+    creation_date=datetime.datetime.now()
+    recipes_list = {}
+
+    def __init__(self, Recipe):
         l1 = []
         l2 = []
         l3 = []
-        recipes_list={'starter': l1, 'lunch': l2, 'dessert': l3}
+        self.recipes_list={'starter': l1, 'lunch': l2, 'dessert': l3}
     def get_recipe_by_name(self, name):
         print(name)
         return Recipe
-        pass
     def get_recipes_by_types(self, recipe_type):
-        recipes_list[Recipe._recipe_type]
+        print(self.recipes_list[recipe_type])
         pass
     def add_recipe(self, recipe):
-        recipes_list[Recipe._recipe_type].append(Recipe._name)
+        self.recipes_list[recipe._recipe_type].append(recipe._name)
+        print(self.recipes_list)
+        self.last_update = datetime.datetime.now()
+        print(self.last_update)
         pass
--- day01/ex00/test.py
+++ day01/ex00/test.py
@@ -1,7 +1,14 @@
 from book import Book
 from recipe import Recipe
 
-tourte = Recipe('a', '1', '10', 'd', 'e', 'dessert')
+tourte = Recipe('tourte', '1', '10', 'd', 'e', 'lunch')
 to_print = str(tourte)
 
 print(to_print)
+book = Book(tourte)
+
+book.get_recipes_by_types('dessert')
+
+book.add_recipe(tourte)
+
+book.get_recipes_by_types('lunch')
