9e006c3ba5f414524a0449eb4bd083bc213b938c
Vincent VIMONT <vinvimo@e1r7p11.42.fr>
2020-01-14 19:07:52 +0100

    day01

--- day01/ex00/book.py
+++ day01/ex00/book.py
@@ -1,15 +1,19 @@
 import sys
+from recipe import Recipe
 
-class Book:
+class Book(Recipe):
     def __init__(name='', recipes_list={}):
-        recipes_list.fromkeys(['starter', 'lunch', 'dessert'])
-
+        l1 = []
+        l2 = []
+        l3 = []
+        recipes_list={'starter': l1, 'lunch': l2, 'dessert': l3}
     def get_recipe_by_name(self, name):
-    """Print a recipe with the name `name` and return the instance"""
+        print(name)
+        return Recipe
         pass
     def get_recipes_by_types(self, recipe_type):
-    """Get all recipe names for a given recipe_type """
+        recipes_list[Recipe._recipe_type]
         pass
     def add_recipe(self, recipe):
-    """Add a recipe to the book and update last_update"""
+        recipes_list[Recipe._recipe_type].append(Recipe._name)
         pass
--- day01/ex00/recipe.py
+++ day01/ex00/recipe.py
@@ -7,38 +7,38 @@ class Recipe:
 #    ingredients = []
 #    description = ''
 #    recipe_type = ''
-    t = ('starter', 'lunch', 'dessert')
+    _t = ('starter', 'lunch', 'dessert')
 
     def __init__(self, name='', cooking_lvl=0, cooking_time=0, \
                 ingredients=[], description='', recipe_type=''):
-        self.n = name
-        self.cl = cooking_lvl
-        self.ct = cooking_time
-        self.i = ingredients
-        self.d = description
-        self.r = recipe_type
+        self._name = name
+        self._cooking_lvl = cooking_lvl
+        self._cooking_time = cooking_time
+        self._ingredients = ingredients
+        self._description = description
+        self._recipe_type = recipe_type
 
     def __str__(self):
-        if (type(self.n) != str):
+        if (type(self._name) != str):
             sys.exit("Input Error: 'name' must be a string\n")
-        if (int(self.cl) < 1 or int(self.cl) > 5):
+        if (int(self._cooking_lvl) < 1 or int(self._cooking_lvl) > 5):
             sys.exit("Input Error: 'cooking_lvl' must be between 1 and 5\n")
-        if (int(self.ct) < 0):
+        if (int(self._cooking_time) < 0):
             sys.exit("Input Error: 'cooking_time' must be a positive number\n")
-        for elem in self.i:
+        for elem in self._ingredients:
             if type(elem) != str:
                 sys.exit("Input Error: 'ingredients' must contain strings only\n")
-        if (type(self.d) != str):
+        if (type(self._description) != str):
             sys.exit("Input Error: 'description' must be a string\n")
-        if (type(self.r) != str) or (not (self.r in self.t)):
+        if (type(self._recipe_type) != str) or (not (self._recipe_type in self._t)):
             sys.exit("Input Error: 'recipe_type' must be either 'starter', " +\
                     "'lunch' or 'dessert'\n")
 
         txt = "Recipe" +\
-        "\nname:            " + self.n +\
-        "\ncooking_lvl:     " + str(self.cl) +\
-        "\ncooking_time:    " + str(self.ct) +\
-        "\ningredients:     " + self.i +\
-        "\ndescription:     " + self.d +\
-        "\nrecipe_type:     " + self.r
+        "\nname:            " + self._name +\
+        "\ncooking_lvl:     " + str(self._cooking_lvl) +\
+        "\ncooking_time:    " + str(self._cooking_time) +\
+        "\ningredients:     " + self._ingredients +\
+        "\ndescription:     " + self._description +\
+        "\nrecipe_type:     " + self._recipe_type
         return txt
