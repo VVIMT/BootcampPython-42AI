1cc44e1996ffab81425a2f96703d24c65ccb72f6
Vincent Vimont <vincent.vimont@live.fr>
2020-01-15 11:07:19 +0100

    day01

---
+++ day01/ex01/game.py
@@ -0,0 +1,11 @@
+import sys
+
+class GotCharacter:
+    first_name = ''
+    is_alive = True
+        def __init__(self, first_name = None, is_alive = True):
+
+
+class Stark(GotCharacter):
+    def __init__(self, first_name = None, is_alive = True):
+        
