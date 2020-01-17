9feba491d01de363b19cb1da636f6332ce1caba6
Vincent VIMONT <vinvimo@e3r5p11.42.fr>
2020-01-17 19:45:13 +0100

    day01

--- day01/ex04/eval.py
+++ day01/ex04/eval.py
@@ -12,15 +12,12 @@ class Evaluator:
         l_zipped = list(zipped)
         sum = 0
         for i in l_zipped:
-            print(i)
-
             if isinstance(i[0], str) and isinstance(i[1], float):
                 sum = sum + (len(i[0]) * i[1])
             elif isinstance(i[1], str) and isinstance(i[0], float):
                 sum = sum + (len(i[1]) * i[0])
             else:
                 raise TypeError
-        print("\nsum = " + str(sum))
         return sum
 
     def enumerate_evaluate(param1, param2):
@@ -31,13 +28,10 @@ class Evaluator:
 
         sum = 0
         for i, (key1, key2) in enumerate(zip(param1, param2)):
-            print(key1)
-
             if isinstance(key1, str) and isinstance(key2, float):
                 sum = sum + (len(key1) * key2)
             elif isinstance(key2, str) and isinstance(key1, float):
                 sum = sum + (key1 * len(key2))
             else:
                 raise TypeError
-        print("\nsum = " + str(sum))
         return sum
