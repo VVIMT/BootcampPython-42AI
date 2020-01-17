351a910420cfc06fe927a9a03f331d371caf95dc
Vincent VIMONT <vinvimo@e3r5p11.42.fr>
2020-01-17 17:01:12 +0100

    day01

--- day01/ex03/generator.py
+++ day01/ex03/generator.py
@@ -1,5 +1,9 @@
 import random
 
+def ordered_set(seq):
+  seen = set()
+  return [x for x in seq if x not in seen and not seen.add(x)]
+
 def generator(text, sep=" ", option=None):
     text = text.split(sep)
 
@@ -9,8 +13,9 @@ def generator(text, sep=" ", option=None):
         random.shuffle(text)
         return text
     elif option == "unique":
-        pass
+        print(text)
+        return list(ordered_set(text))
     elif option == "ordered":
-        pass
+        return sorted(text, key=lambda x:(not x.islower(), x))
     else:
         pass
