55441b4a7b7411399c31b0c342f191faf2da5708
Vincent Vimont <vincent.vimont@live.fr>
2020-01-22 21:13:34 +0100

    day02

--- day02/ft_reduce.py
+++ day02/ft_reduce.py
@@ -1,2 +1,6 @@
 def ft_reduce(function_to_apply, list_of_inputs):
-    pass
+    value = list_of_inputs[0]
+    for element in list_of_inputs[1:]:
+        value = function_to_apply(value, element)
+
+    return value
