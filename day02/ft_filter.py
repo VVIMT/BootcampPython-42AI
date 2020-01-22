1bac22f3486d845bc2c0eb2e8f62bba0b7a4bbbe
Vincent Vimont <vincent.vimont@live.fr>
2020-01-22 19:31:55 +0100

    day02

--- day02/ft_filter.py
+++ day02/ft_filter.py
@@ -1,2 +1,6 @@
 def ft_filter(function_to_apply, list_of_inputs):
-    pass
+    l = []
+    for elem in list_of_inputs:
+        if function_to_apply(elem):
+            l.append(elem)
+    return l
